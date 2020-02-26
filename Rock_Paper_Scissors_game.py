d5a80b2b8a8ed0085095d2ef8e1a1c2ac1324f50
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-02-26 21:26:40 +0200

    Made game easier to read and added comments

--- Rock_Paper_Scissors_game.py
+++ Rock_Paper_Scissors_game.py
@@ -1,84 +1,86 @@
 import random
 
-rounds = 0
-wins = 0
-draws = 0
+rounds = 0  # rounds played
+wins = 0    # wins earned
+draws = 0   # draw situations
 
 
-def ComputerChoice():
+def ComputerChoice():  # Generate computer's choice
     computer = random.randint(0, 2)
     if computer == 0:
-        print("computer chooses: Rock")
+        print("Computer chooses: Rock")
         return computer
     elif computer == 1:
-        print("computer chooses: Paper")
+        print("Computer chooses: Paper")
         return computer
     else:
-        print("computer chooses: Scissors")
+        print("Computer chooses: Scissors")
         return computer
 
 
-def CheckWinner(player, computer):
+def CheckWinner(player, computer):  # Check winner of the round
     global rounds, wins, draws
     if player == "Rock":
         if computer == 0:
-            print("Draw!")
+            print("\nDraw!")
             draws += 1
             rounds += 1
 
         elif computer == 1:
-            print("You lost!")
+            print("\nYou lost!")
             rounds += 1
 
         else:
-            print("You win!")
+            print("\nYou win!")
             wins += 1
             rounds += 1
 
     elif player == "Paper":
         if computer == 0:
-            print("You win!")
+            print("\nYou win!")
             wins += 1
             rounds += 1
 
         elif computer == 1:
-            print("Draw!")
+            print("\nDraw!")
             draws += 1
             rounds += 1
 
         else:
-            print("You lost!")
+            print("\nYou lost!")
             rounds += 1
 
     elif player == "Scissors":
         if computer == 0:
-            print("You lost!")
+            print("\nYou lost!")
             rounds += 1
 
         elif computer == 1:
-            print("You win!")
+            print("\nYou win!")
             wins += 1
             rounds += 1
         else:
-            print("Draw!")
+            print("\nDraw!")
             draws += 1
             rounds += 1
 
 
+# Game running in loop until Quit
 while True:
 
+    # Ask player's choice
     player = input("\nRock, Paper or Scissors? (type Quit to end game): ")
 
-    if player not in ("Rock", "Paper", "Scissors", "Quit"):
+    if player not in ("Rock", "Paper", "Scissors", "Quit"):  # Warning if invalid input
         print("\nPlease choose Rock, Paper or Scissors to play, or Quit to end the game!")
 
-    elif player == "Quit":
+    elif player == "Quit":  # Quit game
         print("\nYou played", rounds, "rounds of which you won",
               wins, "and got a draw", draws, "times.")
         break
 
     else:
-        print("You choose: ", player)
+        print("\nYou choose: ", player)
 
         computer = ComputerChoice()
 
